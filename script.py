26c3b545bfdd8963b05f06d9377404dbad61bf33
Asaph Cadeira <asaphcaldeira@gmail.com>
2025-07-31 23:14:05 -0300

    add test script

---
+++ script.py
@@ -0,0 +1,10 @@
+#Make a program in python to count from 1 to 10
+
+# This script counts from 1 to 10 and prints each number
+#for i in range(1, 11):
+#    print(i)
+
+# This script counts from 1 to 10 and prints each number
+if __name__ == "__main__":
+    for i in range(1, 11):
+        print(i)
